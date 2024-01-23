8a5a3119e9517743ff4e92698f600036f2332316
Sahil Miyawala <chhipasahil163@gmail.com>
2024-01-23 20:05:32 +0530

    First Commit

---
+++ module_1.mjs
@@ -0,0 +1,6 @@
+import aa, { b, c, d } from './module_2.mjs'
+
+console.log(aa);
+console.log(b);
+console.log(c);
+console.log(d); 
---
+++ module_2.mjs
@@ -0,0 +1,9 @@
+const a = "Sahil";
+const b = "Rizzu";
+const c = "Sabu";
+const d = "Iqra";
+
+export default a;
+export {b};
+export {c};
+export {d};
